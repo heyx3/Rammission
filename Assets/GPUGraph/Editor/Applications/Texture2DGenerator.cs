1d287b6e064a814463b953a3decf5098b3dc2218
Billy <manning.w27@gmail.com>
2018-01-27 15:38:02 -0500

    River! (using GPUGraph) Also new meshes

---
+++ Assets/GPUGraph/Editor/Applications/Texture2DGenerator.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEditor;
+using GPUGraph;
+
+
+namespace GPUGraph.Applications
+{
+	[Serializable]
+	public class Texture2DGenerator : TextureGenerator
+	{
+		[MenuItem("Assets/GPU Graph/Generate 2D Texture", false, 4)]
+		public static void ShowWindow()
+		{
+			ScriptableObject.CreateInstance<Texture2DGenerator>().Show();
+		}
+
+
+		public int X = 128,
+				   Y = 128;
+		public bool GenerateNormals;
+		public float NormalStrength = 1.0f;
+
+
+		protected override void OnEnable()
+		{
+			base.OnEnable();
+
+			titleContent = new GUIContent("2D Tex");
+			minSize = new Vector2(200.0f, 300.0f + Y);
+
+			if (HasGraph)
+				GetPreview(true);
+		}
+
+		protected override void OnGUI_BelowGraphSelection()
+		{
+			EditorGUI.BeginChangeCheck();
+			{
+				X = EditorGUILayout.DelayedIntField("Width", X);
+				Y = EditorGUILayout.DelayedIntField("Height", Y);
+			}
+			if (EditorGUI.EndChangeCheck())
+				GetPreview(false);
+			
+			GenerateNormals = EditorGUILayout.Toggle("Normal-map", GenerateNormals);
+			ShowGradient = !GenerateNormals;
+			if (GenerateNormals)
+			{
+				NormalStrength = EditorGUILayout.DelayedFloatField("Strength", NormalStrength);
+			}
+
+			GUILayout.Space(15.0f);
+		}
+
+		protected override void GeneratePreview(ref Texture2D outTex, Material noiseMat)
+		{
+			if (outTex == null || outTex.width != X || outTex.height != Y)
+			{
+				outTex = new Texture2D(X, Y, TextureFormat.ARGB32, false);
+				outTex.wrapMode = TextureWrapMode.Clamp;
+				outTex.filterMode = FilterMode.Point;
+			}
+
+			GraphUtils.GenerateToTexture(RenderTexture.GetTemporary(outTex.width, outTex.height),
+										 noiseMat, outTex, true);
+		}
+		protected override void GenerateTexture()
+		{
+			string savePath = EditorUtility.SaveFilePanel("Choose where to save the texture.",
+														  Application.dataPath, "MyTex.png", "png");
+			if (savePath.Length == 0)
+				return;
+
+			//Write out the texture as a PNG.
+			Texture2D noiseTex = GetPreview(false);
+			if (GenerateNormals)
+				ConvertToNormalMap(noiseTex);
+			try
+			{
+				File.WriteAllBytes(savePath, noiseTex.EncodeToPNG());
+			}
+			catch (Exception e)
+			{
+				Debug.LogError("Unable to save texture to file: " + e.Message);
+			}
+
+			//Finally, open explorer to show the user the texture.
+			EditorUtility.RevealInFinder(StringUtils.FixDirectorySeparators(savePath));
+		}
+		private void ConvertToNormalMap(Texture2D tex)
+		{
+			//Get the texture values as a bumpmap.
+			var pixelColors = tex.GetPixels();
+			var bumpmap = new float[tex.width, tex.height];
+			for (int y = 0; y < tex.height; ++y)
+				for (int x = 0; x < tex.width; ++x)
+					bumpmap[x, y] = pixelColors[x + (y * tex.width)].r;
+
+			//Convert to a normal map and pack it into the texture's colors.
+			for (int y = 0; y < tex.height; ++y)
+			{
+				for (int x = 0; x < tex.width; ++x)
+				{
+					int lessX = (x == 0 ? tex.width - 1 : x - 1),
+						moreX = (x == tex.width - 1 ? 0 : x + 1),
+						lessY = (y == 0 ? tex.height - 1 : y - 1),
+						moreY = (y == tex.height - 1 ? 0 : y + 1);
+					Vector2 heightChangeAlongAxes = new Vector2(bumpmap[moreX, y] - bumpmap[lessX, y],
+																bumpmap[x, moreY] - bumpmap[x, lessY]);
+					var normal = new Vector3(heightChangeAlongAxes.x,
+											 NormalStrength,
+											 heightChangeAlongAxes.y).normalized;
+					pixelColors[x + (y * tex.width)] = new Color(0.5f + (0.5f * normal.x),
+																 0.5f + (0.5f * normal.z),
+																 0.5f + (0.5f * normal.y),
+																 1.0f);
+					continue;
+
+					bool xBig = x > 0,
+						 yBig = y > 0,
+						 xSmall = x < tex.width - 1,
+						 ySmall = y < tex.height - 1;
+					if (xSmall)
+					{
+						float diff = bumpmap[x, y] - bumpmap[x + 1, y];
+						normal += new Vector3(NormalStrength, diff, 0.0f);
+					}
+					if (xBig)
+					{
+						float diff = bumpmap[x, y] - bumpmap[x - 1, y];
+						normal += new Vector3(-NormalStrength, diff, 0.0f);
+					}
+					if (ySmall)
+					{
+						float diff = bumpmap[x, y] - bumpmap[x, y + 1];
+						normal += new Vector3(0.0f, diff, NormalStrength);
+					}
+					if (yBig)
+					{
+						float diff = bumpmap[x, y] - bumpmap[x, y - 1];
+						normal += new Vector3(0.0f, diff, -NormalStrength);
+					}
+
+					normal = normal.normalized;
+					pixelColors[x + (y * tex.width)] = new Color(0.5f + (0.5f * normal.x),
+																 0.5f + (0.5f * normal.y),
+																 0.5f + (0.5f * normal.z),
+																 1.0f);
+				}
+			}
+
+			tex.SetPixels(pixelColors);
+		}
+	}
+}
---
+++ Assets/Materials/RiverShader.shader
@@ -0,0 +1,70 @@
+﻿// Upgrade NOTE: replaced 'mul(UNITY_MATRIX_MVP,*)' with 'UnityObjectToClipPos(*)'
+
+Shader "Water/My River" {
+	Properties {
+		_Color ("Color", Color) = (1,1,1,1)
+        _Normal1 ("Normal 1", 2D) = "white" {}
+        _Normal2 ("Normal 2", 2D) = "white" {}
+
+        _Normal1Pan ("Normal 1 Pan", Vector) = (0,0,0,0)
+        _Normal2Pan ("Normal 2 Pan", Vector) = (0,0,0,0)
+        
+        _FoamTex ("Foam", 2D) = "black" {}
+        _FoamCol ("Foam Color", Color) = (1,1,1,1)
+        _FoamPan ("Foam Pan", Float) = 1.0
+
+		_Glossiness ("Smoothness", Range(0,1)) = 0.5
+		_Metallic ("Metallic", Range(0,1)) = 0.0
+	}
+    SubShader {
+        Tags { "RenderType" = "Opaque" }
+
+        GrabPass {
+            "_Render"
+        }
+
+            CGPROGRAM
+        // Physically based Standard lighting model, and enable shadows on all light types
+        #pragma surface surf Standard fullforwardshadows vertex:vert
+
+        #include "UnityCG.cginc"
+
+        #pragma target 3.0
+
+        struct Input {
+            float2 uv_Normal1;
+            float2 uv_Normal2;
+            float2 uv_FoamTex;
+            float4 grabUV;
+        };
+
+        float4 _Color, _FoamCol;
+        sampler2D _Normal1, _Normal2, _Render, _FoamTex;
+        float2 _Normal1Pan, _Normal2Pan;
+        float _FoamPan;
+        half _Glossiness;
+        half _Metallic;
+
+        void vert(inout appdata_full v, out Input o) {
+            UNITY_INITIALIZE_OUTPUT(Input, o);
+            float4 hpos = UnityObjectToClipPos(v.vertex);
+            o.grabUV = ComputeGrabScreenPos(hpos);
+        }
+        void surf(Input IN, inout SurfaceOutputStandard o) {
+            float3 foam = tex2D(_FoamTex, IN.uv_FoamTex + float2(_FoamPan * _Time.x, 0.0)).rgb;
+            foam *= _FoamCol;
+            o.Albedo = _Color.rgb + foam;
+            o.Metallic = _Metallic;
+            o.Smoothness = _Glossiness;
+            o.Alpha = _Color.a;
+
+            float4 worldRender = tex2Dproj(_Render, UNITY_PROJ_COORD(IN.grabUV));
+
+            float3 norm1 = UnpackNormal(tex2D(_Normal1, IN.uv_Normal1 + (_Normal1Pan * _Time.x))),
+                   norm2 = UnpackNormal(tex2D(_Normal2, IN.uv_Normal2 + (_Normal2Pan * _Time.x)));
+            o.Normal = normalize(max(norm1, norm2));
+        }
+        ENDCG
+    }
+	FallBack "Diffuse"
+}
