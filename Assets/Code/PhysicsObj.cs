0b4950ddaa4c6dd84114d83228116873b6a1bfe7
Billy <manning.w27@gmail.com>
2018-01-26 20:19:42 -0500

    First Demo!

---
+++ Assets/Code/PhysicsObj.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Renderer))]
+[RequireComponent(typeof(Rigidbody))]
+public class PhysicsObj : MonoBehaviour
+{
+	public int PlayerID = -1;
+	public float Acceleration = 10.0f,
+				 TurnSpeed = 6.0f;
+
+	public List<KeyCode> Control_Forward = new List<KeyCode>()
+	{
+		KeyCode.W,
+		KeyCode.UpArrow
+	};
+	public List<KeyCode> Control_Backward = new List<KeyCode>()
+	{
+		KeyCode.S,
+		KeyCode.DownArrow
+	};
+	public List<KeyCode> Control_TurnLeft = new List<KeyCode>()
+	{
+		KeyCode.A,
+		KeyCode.LeftArrow
+	};
+	public List<KeyCode> Control_TurnRight = new List<KeyCode>()
+	{
+		KeyCode.D,
+		KeyCode.RightArrow
+	};
+	public List<Material> PlayerMaterials = new List<Material>();
+
+	private Rigidbody rgd;
+	private Renderer rnd;
+
+	private void Awake()
+	{
+		rgd = GetComponent<Rigidbody>();
+		rnd = GetComponent<Renderer>();
+	}
+	private void FixedUpdate()
+	{
+		rnd.material = PlayerMaterials[PlayerID + 1];
+
+		if (PlayerID < 0)
+			return;
+
+
+		float direction = 0.0f;
+		if (Input.GetKey(Control_Forward[PlayerID]))
+			direction += 1.0f;
+		if (Input.GetKey(Control_Backward[PlayerID]))
+			direction -= 1.0f;
+
+		//rgd.AddForce(transform.forward * direction * Acceleration * Time.deltaTime,
+		//			 ForceMode.Impulse);
+		rgd.velocity += transform.forward * direction * Acceleration * Time.deltaTime;
+		
+		float turnDir = 0.0f;
+		if (Input.GetKey(Control_TurnLeft[PlayerID]))
+			turnDir -= 1.0f;
+		if (Input.GetKey(Control_TurnRight[PlayerID]))
+			turnDir += 1.0f;
+
+		transform.Rotate(new Vector3(0.0f, turnDir * TurnSpeed * Time.deltaTime, 0.0f),
+						 Space.World);
+		var rot = transform.eulerAngles;
+		transform.eulerAngles = new Vector3(0.0f, rot.y, 0.0f);
+	}
+}
---
+++ Assets/Code/Spawner.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+
+public class Spawner : MonoBehaviour
+{
+	public int NToSpawn = 30,
+			   NPlayers = 2,
+			   NPerPlayer = 5;
+	public GameObject Prefab;
+	public float Bounds = 50.0f;
+	public float StartY = 1.0f;
+
+
+	private void Awake()
+	{
+		var rng = new System.Random();
+
+		var objs = new List<GameObject>(NToSpawn);
+		for (int i = 0; i < NToSpawn; ++i)
+		{
+			var obj = Instantiate(Prefab);
+			objs.Add(obj);
+			obj.transform.position = new Vector3(Mathf.Lerp(-Bounds * 0.5f, Bounds * 0.5f, (float)rng.NextDouble()),
+												 StartY,
+												 Mathf.Lerp(-Bounds * 0.5f, Bounds * 0.5f, (float)rng.NextDouble()));
+		}
+
+		for (int playerI = 0; playerI < NPlayers; ++playerI)
+		{
+			for (int objI = 0; objI < NPerPlayer; ++objI)
+			{
+				int i = rng.Next(objs.Count);
+				objs[i].GetComponent<PhysicsObj>().PlayerID = playerI;
+				objs.RemoveAt(i);
+			}
+		}
+
+		foreach (var obj in objs)
+			obj.GetComponent<PhysicsObj>().PlayerID = -1;
+
+		Destroy(this);
+	}
+}
