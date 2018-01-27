c1a0a4476b9990594ceaeeac2b0e428d69f2cd9a
Luke Berry <luke@braid.com>
2018-01-27 17:53:35 -0500

    adjusted targeting system for regularity in menus

--- Assets/Code/MenuRootController.cs
+++ Assets/Code/MenuRootController.cs
@@ -7,12 +7,12 @@ public class MenuRootController : MonoBehaviour {
 	public int firstTarget;
 	private bool deadlerp;
 	private GameObject currentTarget;
+	private float lerpStart;
 	void Awake(){
 	}
 	// Use this for initialization
 	void Start () {
-		this.deadlerp = false;
-		this.currentTarget = this.Targets[this.firstTarget];
+		this.lerpTo(this.firstTarget);
 	}
 	
 	// Update is called once per frame
@@ -21,11 +21,19 @@ public class MenuRootController : MonoBehaviour {
 
 	void FixedUpdate(){
 		if(!deadlerp){
-			this.transform.position = Vector3.Lerp(this.transform.position, this.currentTarget.transform.position, Time.deltaTime);
+			this.transform.position = Vector3.Lerp(this.transform.position, this.currentTarget.transform.position, (Time.deltaTime - this.lerpStart));
+			this.transform.rotation = Quaternion.Lerp(this.transform.rotation, this.currentTarget.transform.rotation, (Time.deltaTime - this.lerpStart));
 			if(Vector3.Distance(this.transform.position, this.currentTarget.transform.position) < 0.001){
 				this.deadlerp = true;
 				Debug.Log("Moved close enough to " + this.currentTarget.name + ", stopping.");
 			}
 		}
+	}
+	
+	public void lerpTo(int targetId){
+		this.currentTarget = this.Targets[targetId];
+		Debug.Log("Moving to " + this.currentTarget.name);
+		this.lerpStart = Time.time;
+		this.deadlerp = false;
 	}
 }
